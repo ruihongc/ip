eb8378ac08365fbfc3096643f3331e66db5975fa
ruihongc <77776793+ruihongc@users.noreply.github.com>
2026-08-14 16:13:21 +0800

    Rename chatbot from Duke to Mochi and implement Level-0 greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mochi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mochi.java
@@ -0,0 +1,20 @@
+/**
+ * Mochi is a chatbot that greets the user and waits for commands.
+ */
+public class Mochi {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = "    __  ___           __    _\n"
+                + "   /  |/  /___  _____/ /_  (_)\n"
+                + "  / /|_/ / __ \\/ ___/ __ \\/ /\n"
+                + " / /  / / /_/ / /__/ / / / /\n"
+                + "/_/  /_/\\____/\\___/_/ /_/_/";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Mochi.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
