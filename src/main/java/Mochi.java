d010fd8cb04e943ae5a14873f793313bf525ac70
ruihongc <77776793+ruihongc@users.noreply.github.com>
2026-08-14 16:32:53 +0800

    Implement mark command to mark tasks as done

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  * Mochi is a chatbot that stores up to 100 tasks entered by the user,
- * lists them on the "list" command, and exits on "bye".
+ * lets the user mark them as done, lists them on the "list" command, and exits on "bye".
  */
 public class Mochi {
     public static void main(String[] args) {
@@ -19,18 +19,28 @@ public class Mochi {
         System.out.println(line);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int count = 0;
         Scanner in = new Scanner(System.in);
         String command = in.nextLine();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 System.out.println(line);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + ".[" + (isDone[i] ? "X" : " ") + "] " + tasks[i]);
                 }
+                System.out.println(line);
+            } else if (command.startsWith("mark ")) {
+                int index = Integer.parseInt(command.substring(5)) - 1;
+                isDone[index] = true;
+                System.out.println(line);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [" + (isDone[index] ? "X" : " ") + "] " + tasks[index]);
                 System.out.println(line);
             } else {
                 tasks[count] = command;
+                isDone[count] = false;
                 count++;
                 System.out.println(line);
                 System.out.println(" added: " + command);
