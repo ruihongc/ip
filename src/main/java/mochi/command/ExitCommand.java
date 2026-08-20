6ee7fd1895b5def8d70eb79f569f3ff63e4a9891
ruihongc <77776793+ruihongc@users.noreply.github.com>
2026-08-20 22:06:57 +0800

    Add Javadoc comments to all public classes and methods (A-JavaDoc)
    Add descriptive Javadoc to public constructors and methods across all
    packages: Mochi, TaskList, Task, Todo, Deadline, Event, TaskType,
    MochiException, and all Command subclasses. Override methods inherit
    parent Javadoc via {@inheritDoc}.
    
    Stretch goal: header comments now cover all non-private classes,
    constructors, and methods.

--- src/main/java/mochi/command/ExitCommand.java
+++ src/main/java/mochi/command/ExitCommand.java
@@ -7,11 +7,13 @@ import mochi.ui.Ui;
  * Exits the chatbot.
  */
 public class ExitCommand extends Command {
+    /** {@inheritDoc} */
     @Override
     public void execute(TaskList tasks, Ui ui) {
         ui.showGoodbye();
     }
 
+    /** {@inheritDoc} */
     @Override
     public boolean isExit() {
         return true;
--- src/main/java/mochi/task/Todo.java
+++ src/main/java/mochi/task/Todo.java
@@ -4,6 +4,11 @@ package mochi.task;
  * Represents a task without any date/time attached to it.
  */
 public class Todo extends Task {
+    /**
+     * Creates a todo task with the given description.
+     *
+     * @param description the task description
+     */
     public Todo(String description) {
         super(TaskType.TODO, description);
     }
